da2a8abd2f71ca6718bd16bb5070c069cd273376
Bruno Dionel <bruno.dionel@livocompany.com>
2025-01-13 08:39:27 -0300

    agrega efecto humo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import InlinePicker from "./components/picker/picker";
 
 import { imageMap, svgMap } from "./constants/images";
+import SmokeBackground from "./components/smokeBakground/smokeBackground";
 
 const values = {
   environment: ["beach", "city", "forest", "mountain"],
@@ -73,33 +74,39 @@ function App() {
   }, [pickerValue]);
 
   return (
-    <section className="p-6 sm:p-10 md:p-12 flex w-full flex-col bg-[#222] text-white min-h-screen">
-      <header className="mb-8 w-full text-center text-3xl font-bold leading-[36px] text-white hover:text-gray-300 transition duration-300 flex items-center justify-center gap-x-4">
-        <h1 className="text-[25px] md:text-[30px]">
-        Weather Wheel
-        </h1>
-        <div className="flex items-center gap-x-2">
-          <img src={currentIcons.environment} className={`w-12 h-12 ${isFadingLeft ? 'animate-fade-up': ''}`}/>
-          <img src={currentIcons.weather} className={`w-12 h-12 ${isFadingRight ? 'animate-fade-left' : '' }`}/>
-        </div>
-      </header>
+    <Fragment>
 
-      <main className="flex w-full items-center justify-center gap-8 md:gap-20 [flex-flow:column]  md:[flex-flow:initial]">
-        <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 h-80 overflow-hidden border-4 border-gray-700 rounded-xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">
-          <img
-            src={currentImage}
-            alt="image"
-            className="w-full h-full object-cover mix-blend-multiply rounded-lg"
+      <SmokeBackground />
+      <section className="p-6 sm:p-10 md:p-12 flex w-full flex-col text-white min-h-screen z-10">
+        
+        
+        <header className="mb-8 w-full text-center text-3xl font-bold leading-[36px] text-white hover:text-gray-300 transition duration-300 flex items-center justify-center gap-x-4">
+          <h1 className="text-[25px] md:text-[30px]">
+          Weather Wheel
+          </h1>
+          <div className="flex items-center gap-x-2">
+            <img src={currentIcons.environment} className={`w-12 h-12 ${isFadingLeft ? 'animate-fade-up': ''}`}/>
+            <img src={currentIcons.weather} className={`w-12 h-12 ${isFadingRight ? 'animate-fade-left' : '' }`}/>
+          </div>
+        </header>
+
+        <main className="flex w-full items-center justify-center gap-8 md:gap-20 [flex-flow:column]  md:[flex-flow:initial]">
+          <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 h-80 overflow-hidden border-4 border-gray-700 rounded-xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl">
+            <img
+              src={currentImage}
+              alt="image"
+              className="w-full h-full object-cover mix-blend-multiply rounded-lg"
+            />
+          </div>
+          <InlinePicker
+            label=""
+            values={values}
+            pickerValue={pickerValue}
+            setPickerValue={handleLeftChange}
           />
-        </div>
-        <InlinePicker
-          label=""
-          values={values}
-          pickerValue={pickerValue}
-          setPickerValue={handleLeftChange}
-        />
-      </main>
-    </section>
+        </main>
+      </section>
+    </Fragment>
   );
 }
 
