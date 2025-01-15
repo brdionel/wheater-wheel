2f641333387f7a16a0f7a232beb30fca5e0d6dbe
Bruno Dionel <bruno.dionel@livocompany.com>
2025-01-15 08:49:33 -0300

    agrega images

--- src/constants/images.js
+++ src/constants/images.js
@@ -8,28 +8,129 @@ import RainySvg from "../assets/svg/rainy.svg"
 import SnowySvg from "../assets/svg/snowy.svg"
 
 export const imageMap = {
-  "beach_rainy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600020/rainy-beach-mountain2_u4dxsm.jpg"],
-  "beach_cloudy": [
-    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600051/wooden-docks-iced-lake-people-ice-skating_wymkvj.jpg"
+  "beach_rainy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600020/rainy-beach-mountain2_u4dxsm.jpg"
   ],
-  "beach_snowy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599998/snowy-beach1_kgjhah.jpg"],
-  "beach_sunny": ["https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/playa-rio_x1nlhh.avif", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/rio-de-janeiro-brazil-november-15-people-enjoying-YVUEU4CCWNH2RCEFZ6EPAJEWBE_gzmy7q.avif"],
+  "beach_cloudy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600051/wooden-docks-iced-lake-people-ice-skating_wymkvj.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940670/beach_sunny_cloudy_11.1_ptpcqv.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940665/beach_cloudy_11.3_ocfelt.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940665/beach_cloudy_11.2_z78rb6.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940665/beach_cloudy_11.1_rbjm7v.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/beache_cloudy_11.1_q1p7ug.png"
+  ],
+  "beach_snowy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599998/snowy-beach1_kgjhah.jpg"
+  ],
+  "beach_sunny": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/playa-rio_x1nlhh.avif", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/rio-de-janeiro-brazil-november-15-people-enjoying-YVUEU4CCWNH2RCEFZ6EPAJEWBE_gzmy7q.avif",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940682/beach_sunny_11.5_c3dffs.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940682/beach_sunny_11.4_kb9ahs.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940671/beach_city_mountain_sunny_11.1_tbfxkn.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940670/beach_sunny_cloudy_11.1_ptpcqv.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940669/beach_sunny_11.1_flr29i.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/beach_sunny_11.3_x0lehb.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/beach_sunny_11.2_vx3pwa.png"
+  ],
 
-  "city_rainy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599865/city-rainy2_qdisfz.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599871/city_rainy_btefax.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599910/rainy-city_eydf5d.jpg"],
-  "city_cloudy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599866/ckoudy-city_bzy14c.jpg"],
-  "city_snowy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599868/city-rainy_bugcap.webp"],
-  "city_sunny": ["https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599869/sunny-city_hxgwgi.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/rio-de-janeiro-brazil-november-15-people-enjoying-YVUEU4CCWNH2RCEFZ6EPAJEWBE_gzmy7q.avif"],
+  "city_rainy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599865/city-rainy2_qdisfz.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599871/city_rainy_btefax.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599910/rainy-city_eydf5d.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940669/city_forest_rainy_11.1_nz2vva.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940668/city_rainy_11.6_tgnvol.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940668/city_rainy_11.7_elgxn2.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_rainy_11.5_dmbjad.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_rainy_11.4_wenaby.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/city_rainy_11.2_x58bih.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/city_rainy_11.1_vfmqko.png"
+  ],
+  "city_cloudy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599866/ckoudy-city_bzy14c.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940683/city_mountain_cloudy_11.3_quhwya.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940682/city_cloudy_11.9_ocbxu6.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940682/city_mountain_cloudy_11.2_pvcjdd.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940681/city_mountain_cloudy_11.1_rxwp1t.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940671/city_cloudy_11.8_auv9af.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940670/city_cloudy_11.7_ctctnk.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940669/city_cloudy_11.6_agnek0.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940669/city_cloudy_11.4_kprzaj.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_cloudy_11.5_cvx2mx.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_cloudy_11.3_vchfwj.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/city_cloudy_11.2_fqa0ck.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/city_cloudy_11.1_meez8h.png"
+  ],
+  "city_snowy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599868/city-rainy_bugcap.webp",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940665/city_snowy_11.1_jmqdsj.png"
+  ],
+  "city_sunny": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599869/sunny-city_hxgwgi.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599900/rio-de-janeiro-brazil-november-15-people-enjoying-YVUEU4CCWNH2RCEFZ6EPAJEWBE_gzmy7q.avif",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940681/city_sunny_11.1_kz0bnq.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940671/beach_city_mountain_sunny_11.1_tbfxkn.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_sunny_11.7_wgeqwo.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940667/city_sunny_11.1_tvbofx.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/city_mountain_sunny_11.1_pcih07.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/city_sunny_11.6_rbbhpk.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940664/city_sunny_11.5_jwpz4u.png"
+  ],
 
-  "forest_rainy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599863/pexels-hikaique-775199_kgomza.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599879/pexels-vladbagacian-1061623_ytbwwp.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599892/pexels-hikaique-775201_uqvuxc.jpg"],
-  "forest_cloudy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599881/pexels-souvenirpixels-1552212_yhqqin.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599892/pexels-hikaique-775201_uqvuxc.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599898/pexels-micah-boerma-327482-1008739_mtxfcc.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599912/pexels-tomfisk-2606532_hnh1d0.jpg"],
-  "forest_snowy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599881/pexels-souvenirpixels-1552212_yhqqin.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599906/pexels-simon73-688660_pfv2vi.jpg"],
-  "forest_sunny": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599866/pexels-japy-1563604_yftcnl.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599868/pexels-pixabay-247600_v23j7p.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599897/pexels-blooddrainer-566496_phdxr2.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599909/pexels-matthew-montrone-230847-1374295_otfrsn.jpg"],
+  "forest_rainy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599863/pexels-hikaique-775199_kgomza.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736599879/pexels-vladbagacian-1061623_ytbwwp.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599892/pexels-hikaique-775201_uqvuxc.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940669/city_forest_rainy_11.1_nz2vva.png"
+  ],
+  "forest_cloudy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599881/pexels-souvenirpixels-1552212_yhqqin.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599892/pexels-hikaique-775201_uqvuxc.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599898/pexels-micah-boerma-327482-1008739_mtxfcc.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599912/pexels-tomfisk-2606532_hnh1d0.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940704/forest_cloudy_11.2_yyvmt2.webp",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940701/forest_cloudy_11.1_s3j5hs.jpg"
+  ],
+  "forest_snowy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599881/pexels-souvenirpixels-1552212_yhqqin.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599906/pexels-simon73-688660_pfv2vi.jpg"
+  ],
+  "forest_sunny": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599866/pexels-japy-1563604_yftcnl.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599868/pexels-pixabay-247600_v23j7p.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599897/pexels-blooddrainer-566496_phdxr2.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599909/pexels-matthew-montrone-230847-1374295_otfrsn.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940702/forest_sunny_11.2_cctofg.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940700/forest_sunny_11.1_hgtaww.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940670/forest_mountain_sunny_11.1_cxhkfs.png"
+  ],
 
-  "mountain_rainy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599981/stephan-seeber-mmzuZhihp30-unsplash_rvvvsu.jpg"],
+  "mountain_rainy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599981/stephan-seeber-mmzuZhihp30-unsplash_rvvvsu.jpg"
+  ],
   "mountain_cloudy": [
-    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600051/wooden-docks-iced-lake-people-ice-skating_wymkvj.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599906/pexels-molly-champion-256223-784148_csitlx.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599920/pexels-vr33s-17125299_iou0qv.jpg"],
-  "mountain_snowy": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599944/pexels-diesgomo-14678594_jzbnte.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599959/pexels-lucasleonelsuarez-18869171_zug2v2.jpg"],
-  "mountain_sunny": ["https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599909/pexels-matthew-montrone-230847-1374295_otfrsn.jpg"],
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736600051/wooden-docks-iced-lake-people-ice-skating_wymkvj.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599906/pexels-molly-champion-256223-784148_csitlx.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599920/pexels-vr33s-17125299_iou0qv.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736940709/mountain_cloudy_smowy_11.2_powsh6.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940703/muntain_cloudy_snowy_11.1_jzbpjd.webp",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940704/mountain_snowy_cloudy_11.1_eii6t4.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940694/mountain_cloudy_11.2_lhh2at.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940691/mountain_cloudy_11.1_atedzi.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940683/city_mountain_cloudy_11.3_quhwya.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940682/city_mountain_cloudy_11.2_pvcjdd.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940681/city_mountain_cloudy_11.1_rxwp1t.png"
+  ],
+  "mountain_snowy": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599944/pexels-diesgomo-14678594_jzbnte.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599959/pexels-lucasleonelsuarez-18869171_zug2v2.jpg", "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940705/mountain_cloudy_11.3_iwlvpm.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940703/muntain_cloudy_snowy_11.1_jzbpjd.webp",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940704/mountain_snowy_cloudy_11.1_eii6t4.jpg"
+  ],
+  "mountain_sunny": [
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736599909/pexels-matthew-montrone-230847-1374295_otfrsn.jpg", 
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/w_800,f_auto,q_auto/v1736940712/mountain_sunny_11.1_bsostc.jpg",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940670/forest_mountain_sunny_11.1_cxhkfs.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940668/mountain_sunny_11.1_p0mbdy.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/mountain_sunny_e6p6qq.png",
+    "https://res.cloudinary.com/dtnixnyfz/image/upload/v1736940666/city_mountain_sunny_11.1_pcih07.png"
+  ],
 }
 
 export const svgMap = {
